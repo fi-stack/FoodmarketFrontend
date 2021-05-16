cb7504389fe1a03cdc10fb0caf6698f84c652c17
rafi-windows <rafi_n_o@github.com>
2021-05-17 06:55:58 +0700

    react navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SplashScreen} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
-  return <SplashScreen></SplashScreen>;
+  return (
+    <NavigationContainer>
+      <SplashScreen />
+    </NavigationContainer>
+  );
 };
 
 export default App;
